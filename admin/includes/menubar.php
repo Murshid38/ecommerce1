a6a1485ea627437c0f66ff5dbb1f435d0a18dcc0
murshidmohamed38@gmail.com <murshidmohamed38@gmail.com>
2023-01-06 09:33:05 +0530

    created powerbi.php page

--- admin/includes/menubar.php
+++ admin/includes/menubar.php
@@ -16,6 +16,7 @@
       <li class="header">REPORTS</li>
       <li><a href="home.php"><i class="fa fa-dashboard"></i> <span>Dashboard</span></a></li>
       <li><a href="sales.php"><i class="fa fa-money"></i> <span>Sales</span></a></li>
+      <li><a href="powerbi.php"><i class="fa fa-bar-chart"></i> <span>Power BI</span></a></li>
       <li class="header">MANAGE</li>
       <li><a href="users.php"><i class="fa fa-users"></i> <span>Users</span></a></li>
       <li class="treeview">
---
+++ admin/powerbi.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>demo</title>
+</head>
+<body>
+  <iframe title="ecomm summary" width="1450" height="750" src="https://app.powerbi.com/reportEmbed?reportId=6a4f6ef5-b1a1-4a30-9b22-a16ecc68d1b0&autoAuth=true&ctid=aa232db2-7a78-4414-a529-33db9124cba7" frameborder="0" allowFullScreen="true"></iframe>
+</body>
+</html>
